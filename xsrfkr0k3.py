41bd1e427ac774eb8007275c6d48a64da31fc082
#̸̥̬̯́̆̾͘Lú̸̺̱̮̖͊̂lzs̶̡͔̉̀eC <112975863+BasHB00M01001@users.noreply.github.com>
2024-05-25 22:03:22 +0200

    Add files via upload

---
+++ xsrfkr0k3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF kroke     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: Kroketaun
+# This module requires XSRFkroke
+# LulZSeC  AntiSeC
+
+from core import main  # import stuff
+main.Engine()  # start the Scanner Engine ;)
